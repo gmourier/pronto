02dd60ec2259dbb952f1a381a8cf02dd0b9322d6
Guillaume Mourier <guillaume@meilisearch.com>
2021-11-21 21:29:17 +0100

    clean import and remove external crate calls

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-extern crate clap;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-#[macro_use]
-extern crate prettytable;
-use prettytable::{Table, format};
 use clap::{Arg, App, SubCommand};
-use std::error::Error;
-use std::fs;
+use serde::{Serialize, Deserialize};
+use prettytable::{Table, Row, Cell, format};
 use std::collections::HashMap;
+use std::error::Error;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::fs;
 use console::Emoji;
-extern crate chrono;
 use chrono::prelude::*;
 
 #[derive(Deserialize, Serialize)]
 struct Task {
     desc: String,
-    done: bool
+    done: bool,
+    created_at: i64
 }
 
 fn read_tasks_data() -> Result<HashMap<i64, Task>, Box<dyn Error>> {
-    let file = fs::File::open("data.json").expect("File should open read only.");
-    let tasks: HashMap<i64, Task> = serde_json::from_reader(file).expect("JSON was not well-formatted.");
+    let file = fs::File::open("data.json").expect("File should open read only");
+    let tasks: HashMap<i64, Task> = serde_json::from_reader(file).expect("JSON was not well-formatted");
     Ok(tasks)
 }
 
 fn write_tasks_data(tasks: &HashMap<i64, Task>) -> Result<(), Box<dyn Error>> {
     let data = serde_json::to_string(&tasks)?;
-    fs::write("data.json", &data).expect("Unable to write data to file.");
+    fs::write("data.json", &data).expect("Unable to write data to file");
     Ok(())
 }
 
@@ -37,7 +31,12 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
     let mut table = Table::new();
     table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
 
-    table.add_row(row!["id", "status", "desc", "created_at"]); //table head
+    table.add_row(Row::new(vec![
+        Cell::new("id"),
+        Cell::new("status"),
+        Cell::new("desc"),
+        Cell::new("created_at")
+    ]));
 
     for (k ,v) in tasks {
         let icon;
@@ -48,11 +47,16 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
             icon = Emoji("❌", "todo");
         }
 
-        let naive = NaiveDateTime::from_timestamp(*k, 0);
+        let naive = NaiveDateTime::from_timestamp(v.created_at, 0);
         let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
         let created_at = datetime.format("%Y-%m-%d %H:%M:%S");
 
-        table.add_row(row![k, icon, v.desc, created_at]);
+        table.add_row(Row::new(vec![
+            Cell::new(&k.to_string()),
+            Cell::new(&icon.to_string()),
+            Cell::new(&v.desc),
+            Cell::new(&created_at.to_string())
+        ]));
     }
 
     table.printstd();
@@ -60,6 +64,12 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
 
 fn main() {
     let mut tasks = read_tasks_data().unwrap();
+
+    //set current auto-increment for task id
+    let mut c: i64 = 0;
+    if let Some(&max) = tasks.keys().max(){
+        c = max;
+    }
 
     let matches = App::new("Pronto")
         .about("Manage your todo-list in your terminal")
@@ -105,6 +115,20 @@ fn main() {
         .subcommand(SubCommand::with_name("cl")
             .about("Clear tasks list")
         )
+        .subcommand(SubCommand::with_name("s")
+            .about("Search for a substring in a task desc")
+            .arg(Arg::with_name("sub_string")
+                .help("Search pattern")
+                .required(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("e")
+            .about("Export tasks list to a the specified format")
+            .arg(Arg::with_name("format")
+                .help("Format to export")
+                .required(true)
+            )
+        )
         .get_matches();
 
     if let Some(_) = matches.subcommand_matches("l") {
@@ -114,7 +138,14 @@ fn main() {
     if let Some(add) = matches.subcommand_matches("a") {
         let desc: String = add.value_of("desc").unwrap().into();
 
-        tasks.insert(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64, Task { desc: desc, done: false });
+        tasks.insert(
+            c + 1,
+            Task {
+                desc: desc,
+                done: false,
+                created_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64
+            }
+        );
 
         write_tasks_data(&tasks).unwrap_or(());
         print_tasks(&tasks);
