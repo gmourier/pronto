a9afb80ce5b97258cd8bc0ff8e07f7a63d0d498f
Guillaume Mourier <guillaume@meilisearch.com>
2021-11-21 22:33:02 +0100

    Replace clap boilerplate by structopts

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use clap::{Arg, App, SubCommand};
-use serde::{Serialize, Deserialize};
-use prettytable::{Table, Row, Cell, format};
+mod options;
+
+use serde::{ Serialize, Deserialize };
+use prettytable::{ Table, Row, Cell, format };
 use std::collections::HashMap;
 use std::error::Error;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{ SystemTime, UNIX_EPOCH };
 use std::fs;
 use console::Emoji;
 use chrono::prelude::*;
 
+use options::{ Command, Options };
+use structopt::StructOpt;
+
 #[derive(Deserialize, Serialize)]
 struct Task {
-    desc: String,
-    done: bool,
+    description: String,
+    completed: bool,
     created_at: i64
 }
 
@@ -40,7 +44,7 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
 
     for (k ,v) in tasks {
         let icon;
-        if v.done {
+        if v.completed {
             icon = Emoji("✅", "done");
         }
         else {
@@ -54,7 +58,7 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
         table.add_row(Row::new(vec![
             Cell::new(&k.to_string()),
             Cell::new(&icon.to_string()),
-            Cell::new(&v.desc),
+            Cell::new(&v.description),
             Cell::new(&created_at.to_string())
         ]));
     }
@@ -62,131 +66,85 @@ fn print_tasks(tasks: &HashMap<i64, Task>) {
     table.printstd();
 }
 
-fn main() {
-    let mut tasks = read_tasks_data().unwrap();
-
+fn add(tasks: &mut HashMap<i64, Task>, description: String) {
     //set current auto-increment for task id
     let mut c: i64 = 0;
     if let Some(&max) = tasks.keys().max(){
         c = max;
     }
 
-    let matches = App::new("Pronto")
-        .about("Manage your todo-list in your terminal")
-        .version("v0.1.0")
-        .author("Guillaume Mourier (@gmourier on GitHub)")
-        .subcommand(SubCommand::with_name("l")
-            .about("List tasks to-do")
-        )
-        .subcommand(SubCommand::with_name("a")
-            .about("Add a task")
-            .arg(Arg::with_name("desc")
-                .help("Sets the description for the new task")
-                .required(true)
-            )
-        )
-        .subcommand(SubCommand::with_name("r")
-            .about("Rename a task")
-            .arg(Arg::with_name("id")
-                .help("Identifer for the task to be updated")
-                .required(true)
-                .index(1)
-            )
-            .arg(Arg::with_name("desc")
-                .help("Sets the description for a task")
-                .required(true)
-                .index(2)
-            )
-        )
-        .subcommand(SubCommand::with_name("c")
-            .about("Mark a task as completed")
-            .arg(Arg::with_name("id")
-                .help("Identifier marking the task to be completed")
-                .required(true)
-            )
-        )
-        .subcommand(SubCommand::with_name("d")
-            .about("Delete a task")
-            .arg(Arg::with_name("id")
-                .help("Identifier marking the task to be deleted")
-                .required(true)
-            )
-        )
-        .subcommand(SubCommand::with_name("cl")
-            .about("Clear tasks list")
-        )
-        .subcommand(SubCommand::with_name("s")
-            .about("Search for a substring in a task desc")
-            .arg(Arg::with_name("sub_string")
-                .help("Search pattern")
-                .required(true)
-            )
-        )
-        .subcommand(SubCommand::with_name("e")
-            .about("Export tasks list to a the specified format")
-            .arg(Arg::with_name("format")
-                .help("Format to export")
-                .required(true)
-            )
-        )
-        .get_matches();
-
-    if let Some(_) = matches.subcommand_matches("l") {
-        print_tasks(&tasks);
-    }
-
-    if let Some(add) = matches.subcommand_matches("a") {
-        let desc: String = add.value_of("desc").unwrap().into();
-
-        tasks.insert(
-            c + 1,
-            Task {
-                desc: desc,
-                done: false,
-                created_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64
-            }
-        );
-
-        write_tasks_data(&tasks).unwrap_or(());
-        print_tasks(&tasks);
-    }
-
-    if let Some(rename) = matches.subcommand_matches("r") {
-        let id: i64 = rename.value_of("id").unwrap().parse::<i64>().expect("can't parse the identifier");
-        let desc: String = rename.value_of("desc").unwrap().into();
-
-        if let Some(task) = tasks.get_mut(&id) {
-            task.desc = desc;
+    tasks.insert(
+        c + 1,
+        Task {
+            description: description,
+            completed: false,
+            created_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64
         }
+    );
+}
 
-        write_tasks_data(&tasks).unwrap_or(());
-        print_tasks(&tasks);
+fn update(tasks: &mut HashMap<i64, Task>, id: i64, description: String) {
+    if let Some(task) = tasks.get_mut(&id) {
+        task.description = description;
     }
+}
 
-    if let Some(complete) = matches.subcommand_matches("c") {
-        let id: i64 = complete.value_of("id").unwrap().parse::<i64>().expect("can't parse the identifier");
+fn complete(tasks: &mut HashMap<i64, Task>, id: i64) {
+    if let Some(task) = tasks.get_mut(&id) {
+        task.completed = true;
+    }
+}
 
-        if let Some(task) = tasks.get_mut(&id) {
-            task.done = true;
+fn delete(tasks: &mut HashMap<i64, Task>, id: i64) {
+    tasks.remove(&id);
+}
+
+fn clear(tasks: &mut HashMap<i64, Task>) {
+    tasks.clear();
+}
+
+fn main() {
+    //Load tasks from file at startup
+    let mut tasks = read_tasks_data().unwrap();
+
+    let opt = Options::from_args();
+    match opt.command {
+        Command::List => {
+            print_tasks(&tasks);
+        },
+        Command::Add {
+            description
+        } => {
+            add(&mut tasks, description);
+            write_tasks_data(&tasks).unwrap_or(());
+            print_tasks(&tasks);
+        },
+        Command::Update {
+            id,
+            description
+        } => {
+            update(&mut tasks, id, description);
+            write_tasks_data(&tasks).unwrap_or(());
+            print_tasks(&tasks);
+        },
+        Command::Delete {
+            id
+        } => {
+            delete(&mut tasks, id);
+            write_tasks_data(&tasks).unwrap_or(());
+            print_tasks(&tasks);
+        },
+        Command::Complete {
+            id
+        } => {
+            complete(&mut tasks, id);
+            write_tasks_data(&tasks).unwrap_or(());
+            print_tasks(&tasks);
+        },
+        Command::Clear => {
+            clear(&mut tasks);
+            write_tasks_data(&tasks).unwrap_or(());
+            print_tasks(&tasks);
         }
-
-        write_tasks_data(&tasks).unwrap_or(());
-        print_tasks(&tasks);
-    }
-
-    if let Some(delete) = matches.subcommand_matches("d") {
-        let id: i64 = delete.value_of("id").unwrap().parse::<i64>().expect("can't parse the identifier");
-
-        tasks.remove(&id);
-
-        write_tasks_data(&tasks).unwrap_or(());
-        print_tasks(&tasks);
-    }
-
-    if let Some(_) = matches.subcommand_matches("cl") {
-        tasks.clear();
-
-        write_tasks_data(&tasks).unwrap_or(());
-        print_tasks(&tasks);
     }
 }
---
+++ src/options.rs
@@ -0,0 +1,38 @@
+use structopt::*;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Todo-list management with the command-line interface written in rust")]
+pub struct Options {
+    #[structopt(subcommand)]
+    pub command: Command
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Command {
+    /// List all tasks
+    List,
+    /// Add a new task
+    Add {
+        /// Description for the task
+        description: String
+    },
+    /// Update a task
+    Update {
+        /// Identifier for the task to update
+        id: i64,
+        /// Description for the task
+        description: String
+    },
+    /// Delete a task
+    Delete {
+        /// Identifier for the task to delete
+        id: i64
+    },
+    /// Mark a task as completed
+    Complete {
+        /// Identifier for the task to complete
+        id: i64
+    },
+    /// Clear all task
+    Clear
+}
